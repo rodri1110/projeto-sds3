d08292dd3e3a2860c18eba846c119d03a5f871e0
rodrigo <rodri.oliveira1110@gmail.com>
2021-05-09 01:20:54 -0300

    project created

---
+++ backend/src/test/java/com/rodrigo/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
