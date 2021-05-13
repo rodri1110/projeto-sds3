aa8a4efd9914ea066a984670fdbdef46aa327e06
rodrigo <rodri.oliveira1110@gmail.com>
2021-05-13 20:28:47 -0300

    clean imports DonutChart

--- frontend/src/componentes/DonutChart/index.tsx
+++ frontend/src/componentes/DonutChart/index.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
-import { matchPath } from 'react-router';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
